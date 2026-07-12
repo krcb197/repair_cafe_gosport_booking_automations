18a680515522ea203636635aa23a369bb77213d3
Keith Brady <34693973+krcb197@users.noreply.github.com>
2026-07-12 16:57:44 +0100

    New script to make a list of event dates. Closes #8

--- src/py_rcg_booking_automation/__init__.py
+++ src/py_rcg_booking_automation/__init__.py
@@ -2,3 +2,5 @@ from .rcg_anonomised_summary import EventSummaryReport
 from .rcg_repair_forms import RepairForms
 from .rcg_supporter_crm_export import SupporterCRMExporter
 from .rcg_tito import RCG_TITO_API
+from .date_operations import second_saturday
+from .date_operations import event_dates
--- src/py_rcg_booking_automation/date_operations.py
+++ src/py_rcg_booking_automation/date_operations.py
@@ -4,6 +4,7 @@ the 2nd saturday of the month
 """
 import datetime
 import calendar
+from collections.abc import Iterator
 
 def second_saturday(year, month) -> datetime.date:
     """
@@ -28,3 +29,29 @@ def previous_second_saturday(month, year) -> datetime.date:
     return second_saturday(year=year, month=month)
 
 one_min_after_midnight = datetime.time(hour=0, minute=1, second=0, tzinfo=datetime.timezone.utc)
+
+def iter_months(start_date: datetime.date, end_date: datetime.date):
+    """
+    An iterator that yields the month and year between a start date and an end date,
+    excluding August (because Repair Cafe Gosport takes a break that month)
+    """
+    year = start_date.year
+    month = start_date.month
+
+    while (year, month) <= (end_date.year, end_date.month):
+        if month != 8:
+            # Repair Cafe Gosport does not operate in August
+            yield year, month
+
+        month += 1
+        if month > 12:
+            month = 1
+            year += 1
+
+
+def event_dates(start_date: datetime.date, end_date: datetime.date) -> Iterator[datetime.date]:
+    """
+    An Iterator that yields the dates of Repair Cafe Gosport Events between two dates
+    """
+    for year, month in iter_months(start_date, end_date):
+        yield second_saturday(year=year, month=month)
